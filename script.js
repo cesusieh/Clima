3d3168f388e11b33a228912bbdaa9d54b44cfa49
cesusieh <cesux3@gmail.com>
2023-11-21 22:11:06 -0300

    2.0

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-const API_KEY = '85e840bbcc583778a50895619e12434f'
+const API_KEY = '85e840bbcc583778a50895619e12434f';
 const form = document.getElementById('form');
 const input = document.getElementById('search_cidade');
-
-var historico = [];
 
 const infoCidade = document.getElementById('info_cidade');
 const infoPais = document.getElementById('info_pais');
@@ -15,6 +13,8 @@ const maxTemp = document.getElementById('info_max');
 const pressao = document.getElementById('info_press');
 const umidade = document.getElementById('info_umi');
 const velVento = document.getElementById('info_vel_vento');
+const fixButton = document.getElementById('fix_button');
+const areaFix = document.getElementById('area_fixo');
 
 const callApi = async (cidade,key) => {
     const APIResponse = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`);
@@ -28,9 +28,6 @@ const renderInfo = async(cidade) => {
     const data = await callApi(cidade, API_KEY);
     input.value = '';
     if(data){
-
-        historico.unshift(cidade);
-
         infoCidade.innerHTML = data.name;
         infoPais.innerHTML = data['sys']['country'];
         temperatura.innerHTML = `TEMPERATURA: ${Math.trunc((data['main']['temp'] - 273.1))}°C`;
@@ -39,9 +36,7 @@ const renderInfo = async(cidade) => {
         maxTemp.innerHTML = `MÁXIMA: ${Math.trunc((data['main']['temp_max'] - 273.1))}°C`;
         pressao.innerHTML = `PRESSÃO: ${data['main']['pressure']} hPa`;
         umidade.innerHTML = `UMIDADE: ${data['main']['humidity']}%`;
-        velVento.innerHTML = `VENTO: ${Math.trunc((data['wind']['speed'])*3.6) } km/h`
-
-        console.log(historico);
+        velVento.innerHTML = `VENTO: ${Math.trunc((data['wind']['speed'])*3.6) } km/h`;
     } else {
         infoCidade.innerHTML = 'Cidade não encontrada';
         infoPais.innerHTML = '';
@@ -55,13 +50,72 @@ const renderInfo = async(cidade) => {
     }
 }
 
+/* Fixos */
+
+var fix = JSON.parse(localStorage.getItem("fixados")) || [];
+var lastSearch = "";
+var nFix = fix.length;
+
+function verifyFix(cidade){
+    for(item in fix){
+        if(cidade.toUpperCase() === fix[item]["nome"]){
+            fixButton.innerHTML = "Desfixar";
+            break;
+        } else {
+            fixButton.innerHTML = "Fixar";
+        }
+    }
+}
+
+
+function addFix(cidade){
+    if(cidade != ""){
+        fix[nFix] = {nome: cidade.toUpperCase()};
+        localStorage.setItem("fixados", JSON.stringify(fix));
+        renderFix();
+        nFix++;
+        fixButton.innerHTML = "Desfixar";
+    }
+}
+
+function removeFix(cidade){
+    let x = '';
+    for(item in fix){
+        if(cidade.toUpperCase() == fix[item]["nome"]){
+            x = item;
+            break;
+        }
+    }
+    fix.splice(x, 1);
+    nFix--;
+    localStorage.setItem("fixados", JSON.stringify(fix));
+    fixButton.innerHTML = "Fixar";
+    renderFix()
+}
+
+fixButton.addEventListener("click", () => {
+    if(fixButton.innerHTML === "Fixar"){
+        addFix(lastSearch);
+    } else if (fixButton.innerHTML === "Desfixar"){
+        removeFix(lastSearch);
+    }
+})
+
+async function renderFix(){
+    areaFix.innerHTML = `<h3 class="info_titulo">Fixos</h3>`
+    for(item in fix){
+        const data = await callApi(fix[item]["nome"], API_KEY);
+        areaFix.innerHTML += `<div class="info_texto">${fix[item]["nome"]} - ${ Math.trunc((data['main']['temp'] - 273.1)) }°C</div>`
+    }
+}
+
+/* Search submit */
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+    lastSearch = input.value;
+    verifyFix(lastSearch);
     renderInfo(input.value);
 });
 
-historico.forEach(lugar => {
-
-    console.log(lugar);
-    
-});
+renderFix();
